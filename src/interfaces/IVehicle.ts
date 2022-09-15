a15eb5da22706cd7c59d0957d890fae08e5c694a
AugustoMagalhaes <augustommg@gmail.com>
2022-09-15 02:29:19 -0300

    feat: implementa interface IVehicle.

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Model must be a string',
+    })
+    .min(3, { message: 'Model must be 3 or more characters long' }),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be an integer',
+    })
+    .int()
+    .refine((year) => year >= 1900 && year <= 20022, {
+      message: 'Year must be between 1900 and 2022',
+    }),
+  color: z
+    .string({
+      required_error: 'Color is required',
+      invalid_type_error: 'Color must be a string',
+    })
+    .min(3, { message: 'Color must be 3 or more characters long' }),
+  status: z.optional(z.boolean()),
+  buyValue: z
+    .number({
+      required_error: 'BuyValue is required',
+      invalid_type_error: 'BuyValue must be an integer',
+    })
+    .int(),
+});
+
+type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export { vehicleZodSchema, IVehicle };
