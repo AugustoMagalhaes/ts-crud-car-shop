e8668efb18e4b867b8c2bf2486b8b98495415746
AugustoMagalhaes <augustommg@gmail.com>
2022-09-16 04:07:01 -0300

    feat: adiciona rota carRoute ao app.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import carRoute from './routes/Car.route';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', carRoute);
 
 export default app;
