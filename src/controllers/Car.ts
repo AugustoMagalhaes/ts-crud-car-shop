1d4a361cf65df14295477b5bd56e0b0e632b2f83
AugustoMagalhaes <augustommg@gmail.com>
2022-09-16 04:06:14 -0300

    feat: implementa controller Car com metodo create.

---
+++ src/controllers/Car.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import { IService } from '../interfaces/IService';
+
+class CarController {
+  private _service: IService<ICar>;
+
+  constructor(service: IService<ICar>) {
+    this._service = service;
+  }
+
+  public async create(req: Request, res: Response<ICar>) {
+    const { model, year, color, status, buyValue, doorsQty, seatsQty } = req.body;
+    const car = { model, year, color, status, buyValue, doorsQty, seatsQty };
+    const created = await this._service.create(car);
+
+    return res.status(201).json(created);
+  }
+}
+
+export default CarController;
