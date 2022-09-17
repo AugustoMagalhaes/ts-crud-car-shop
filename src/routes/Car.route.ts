f2b15f0d5422d3766150bba59cede6e290f29609
AugustoMagalhaes <augustommg@gmail.com>
2022-09-17 03:46:57 -0300

    feat: implementa rota DELETE '/:id' em carRouter.

--- src/routes/Car.route.ts
+++ src/routes/Car.route.ts
@@ -15,6 +15,7 @@ carRoute.post('/', (req, res) => carController.create(req, res));
 carRoute.get('/', (_req, res) => carController.read(_req, res));
 carRoute.get('/:id', (req, res) => carController.readOne(req, res));
 carRoute.put('/:id', (req, res) => carController.update(req, res));
+carRoute.delete('/:id', (req, res) => carController.delete(req, res));
 
 carRoute.use(errorHandler);
 
