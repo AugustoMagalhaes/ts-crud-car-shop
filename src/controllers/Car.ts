b7b0a27bc061f361de84c22f403f227c5d867e80
AugustoMagalhaes <augustommg@gmail.com>
2022-09-17 03:41:30 -0300

    feat: implementa metodo update em CarController.

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -12,6 +12,7 @@ class CarController {
   public async create(req: Request, res: Response<ICar>) {
     const { model, year, color, status, buyValue, doorsQty, seatsQty } = req.body;
     const car = { model, year, color, status, buyValue, doorsQty, seatsQty };
+    
     const created = await this._service.create(car);
 
     return res.status(201).json(created);
@@ -25,7 +26,16 @@ class CarController {
   public async readOne(req: Request, res: Response<ICar>) {
     const { id } = req.params;
     const car = await this._service.readOne(id);
+    
     return res.status(200).json(car);
+  }
+
+  public async update(req: Request, res: Response<ICar>) {
+    const { id } = req.params;
+    console.log('id', id);
+    const updatedCar = await this._service.update(id, req.body);
+    
+    return res.status(200).json(updatedCar);
   }
 }
 
