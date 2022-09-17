e6d7334299f5241ddd837f27bbc1093ceec6ccee
AugustoMagalhaes <augustommg@gmail.com>
2022-09-17 03:46:29 -0300

    feat: implementa metodo delete em CarController.

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -37,6 +37,13 @@ class CarController {
     
     return res.status(200).json(updatedCar);
   }
+
+  public async delete(req: Request, res: Response<ICar>) {
+    const { id } = req.params;
+    await this._service.delete(id);
+
+    return res.status(204).send();
+  }
 }
 
 export default CarController;
